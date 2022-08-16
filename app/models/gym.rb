ddb833ba652bde1a75bb83398639dfb193edc61a
Victor Turitzin <turitzinv@gmail.com>
2022-08-16 16:35:00 -0400

    Added gym, user, review models and migrations.

---
+++ app/models/gym.rb
@@ -0,0 +1,4 @@
+class Gym < ApplicationRecord
+  has_many :reviews
+  has_many :users, through: :reviews
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :reviews
+  has_many :gyms, through: :reviews
+
+end
