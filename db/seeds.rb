657bb71882a21a87551e675618171cff61321294
Victor Turitzin <turitzinv@gmail.com>
2022-08-18 21:24:12 -0400

    Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "🌱 Seeding Gyms and Reviews..."
+
+Gym.create(name: "Boosted Fitness", address: "12 Plainview Drive")
+Gym.create(name: "24/7 Gym", address: "55 Lake Plaza")
+Gym.create(name: "Full Throttle Fitness", address: "34 Ocean Lane")
+
+User.create(username: "John Smith")
+User.create(username: "Mary Thompson")
+User.create(username: "Larry Phillips")
+
+Review.create(
+  description: "Gym is spacious, a lot to work with and there's a pool!",
+  user_id: 1,
+  gym_id: 1
+)
+
+Review.create(
+  description: "Gym is okay, the hours are great but it can get pretty busy even during off peak hours.",
+  user_id: 2,
+  gym_id: 2
+)
+
+Review.create(
+  description: "Staff is nice and accomodating. Wish there were more dumbbells to work with, often find myself waiting for someone.",
+  user_id: 3,
+  gym_id: 3
+)
+
+puts "✅ Done seeding!"
