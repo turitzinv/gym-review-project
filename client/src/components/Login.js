eb0873f585de8430299ad44a6487bf95e07e1a0e
Victor Turitzin <turitzinv@gmail.com>
2022-09-05 19:54:45 -0400

    Added css to login.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,7 +7,7 @@ const Login = ({ setUser }) => {
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
-  let history = useHistory()
+  let history = useHistory();
 
   // function onClick(){
   //   history.push("/signuppage")
@@ -23,73 +23,74 @@ const Login = ({ setUser }) => {
       body: JSON.stringify({ username, password }),
     }).then((resp) => {
       if (resp.ok) {
-        resp.json().then((user) => setUser(user))
-        history.push("/")
+        resp.json().then((user) => setUser(user));
+        history.push("/");
       } else {
-        resp.json().then((err) => setErrors(err.errors))
+        resp.json().then((err) => setErrors(err.errors));
       }
-    })
+    });
   }
 
   function handleSignUp(event) {
-    event.preventDefault()
+    event.preventDefault();
     fetch("/signup", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password}),
+      body: JSON.stringify({ username, password }),
     }).then((resp) => {
       if (resp.ok) {
-        resp.json().then(setUser)
-        history.push("/")
+        resp.json().then(setUser);
+        history.push("/");
       } else {
-        resp.json().then((err) => setErrors(err.errors))
+        resp.json().then((err) => setErrors(err.errors));
       }
-    })
+    });
   }
 
   function errorRender() {
     if (errors instanceof Array) {
-      return errors.map((error) => 
-      <Error key={error} error={error} />
-      )
+      return errors.map((error) => <Error key={error} error={error} />);
     } else {
-      return null
+      return null;
     }
   }
 
-
   return (
-    <form id="login-form" onSubmit={handleLogin}>
-      <div class="mb-3">
-        <input
-          type="text"
-          class="form-control"
-          id="username"
-          placeholder="Username"
-          value = {username}
-          onChange = {(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div class="mb-3">
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          placeholder="Password"
-          value = {password}
-          onChange = {(e) => setPassword(e.target.value)}
-        />
-      </div>
-      {errorRender()}
-      <button id="login-button" type="submit" class="btn btn-dark">
-        Log In
-      </button>
-      <button id="signup-button" onClick={handleSignUp} class="btn btn-dark">
-        Sign Up
-      </button>
-    </form>
+    <div>
+      <h1 id="login-gymview">GymView</h1>
+      <p id="login-message">Please Login or Create an Account</p>
+      <form id="login-form" onSubmit={handleLogin}>
+        <div class="mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {errorRender()}
+        <button id="login-button" type="submit">
+          Log In
+        </button>
+        <button id="signup-button" onClick={handleSignUp}>
+          Sign Up
+        </button>
+      </form>
+    </div>
   );
 };
 
